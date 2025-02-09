9cc83c5d113b0e370f5bbe50866b7ae4f91751a5
LirWe786 <primak2020@gmail.com>
2025-02-09 09:26:42 +0300

    background animation

---
+++ main.js
@@ -0,0 +1,42 @@
+function sendMessage() {
+    const message = document.getElementById('message').value;
+    if (message.trim() === "") {
+        alert("Напиши что-нибудь милое!");
+    } else {
+        alert("Сообщение отправлено: " + message);
+  
+    }
+}
+const heartsContainer = document.querySelector('.hearts-container');
+
+
+
+
+
+// анимация сердечек
+function createHeart() {
+    const heart = document.createElement('div');
+    heart.classList.add('heart');
+
+    // Рандомный размер сердечка
+    const size = Math.random() * 30 + 10; // от 10px до 40px
+    heart.style.width = `${size}px`;
+    heart.style.height = `${size}px`;
+
+    // Рандомная позиция по горизонтали
+    heart.style.left = Math.random() * 100 + 'vw';
+
+    // Рандомная скорость падения
+    const duration = Math.random() * 3 + 2; // от 2s до 5s
+    heart.style.animationDuration = `${duration}s`;
+
+    // Удаление сердечка после завершения анимации
+    heart.addEventListener('animationend', () => {
+        heart.remove();
+    });
+
+    heartsContainer.appendChild(heart);
+}
+
+// Создаем новые сердечки каждые 300ms
+setInterval(createHeart, 300);
